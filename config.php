e0a51226a53344a68665946439b54e8381c534eb
Haydée Laiz <haydeelgp@gmail.com>
2024-11-27 19:26:06 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'dados_doceria';
+?>
+
+// Verificar se a conexão foi efetuada
+// $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// if ($conexao->connect_errno) {
+// echo "erro";
+// } else {
+// echo "deu certo";
+// }
